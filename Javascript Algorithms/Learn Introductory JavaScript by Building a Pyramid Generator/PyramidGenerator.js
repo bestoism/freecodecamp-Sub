2a77abc0054c7e86e1dbcc640623792426b8b2de
bestoism <bestoisme24@gmail.com>
2025-05-23 00:26:49 +0700

    Initial commit

--- Javascript Algorithms/Learn Introductory JavaScript by Building a Pyramid Generator/PyramidGenerator.js
+++ Javascript Algorithms/Learn Introductory JavaScript by Building a Pyramid Generator/PyramidGenerator.js
@@ -1,17 +1,21 @@
 const character = "#";
 const count = 8;
 const rows = [];
+let inverted = true;
 
 function padRow(rowNumber, rowCount) {
-  const spaces = " ".repeat(rowCount - rowNumber);
-  return spaces + character.repeat(rowNumber) + spaces;
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(padRow(i + 1, count));
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
 }
 
-let result = "";
+let result = ""
 
 for (const row of rows) {
   result = result + row + "\n";
